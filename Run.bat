4458484aa51c1d29262199d4c435659b542f7a6d
bwanaaskari <synk63@gmail.com>
2017-06-17 22:05:36 -0700

    batch file

---
+++ Run.bat
@@ -0,0 +1,4 @@
+REM cd c:\temp\winsock-test
+REM test.exe api.openweathermap.org 80 GET "/data/2.5/weather?q=Vancouver&mode=html&appid=9006f0a9426cb10a575309f678b30f0a"
+REM "C:\Program Files\Internet Explorer\iexplore.exe" file:///c:\temp\winsock-test\test.html
+pause
