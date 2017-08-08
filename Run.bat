337c215e4bc53cb95efd93bf858c2d582b511b54
bwanaaskari <synk63@gmail.com>
2017-08-08 16:25:26 +0000

    changed target city to yvr

--- Run.bat
+++ Run.bat
@@ -1,4 +1,9 @@
-CALL .\test.exe api.openweathermap.org 80 GET "/data/2.5/weather?q=Phoenix&mode=html&appid=9006f0a9426cb10a575309f678b30f0a"
+:: 
+::Date		Developer	JIRA		Comments
+::----		---------	----		--------
+::08Jun2017	ts			jira-1234	created
+
+CALL .\test.exe api.openweathermap.org 80 GET "/data/2.5/weather?q=Vancouver&mode=html&appid=9006f0a9426cb10a575309f678b30f0a"
 start iexplore file://%CD%\test.html
 pause
 pause
