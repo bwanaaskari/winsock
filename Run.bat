c618a422e01a3938d02da790bfb00251dc099218
bwanaaskari <synk63@gmail.com>
2017-08-16 17:38:10 +0000

    restore prev version

--- Run.bat
+++ Run.bat
@@ -3,7 +3,7 @@
 ::----		---------	----		--------
 ::08Jun2017	ts			jira-1234	created
 
-CALL .\test.exe api.openweathermap.org 80 GET "/data/2.5/weather?q=Seattle&mode=html&appid=9006f0a9426cb10a575309f678b30f0a"
+CALL .\test.exe api.openweathermap.org 80 GET "/data/2.5/weather?q=Victoria&mode=html&appid=9006f0a9426cb10a575309f678b30f0a"
 start iexplore file://%CD%\test.html
 pause
 pause
